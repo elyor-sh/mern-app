9df5f99b6b9e52b632de35d592680acdba9221c7
Elyor <elyor.sh.99@mail.ru>
2021-10-30 14:42:47 +0300

    #1 initial project

---
+++ index.js
@@ -0,0 +1,26 @@
+const  express = require('express')
+const  mongoose = require('mongoose')
+const  config =  require('config')
+
+const app = express()
+
+const PORT = config.get('port') || 5000
+
+async function start() {
+
+    try {
+        
+        await mongoose.connect(config.get('mongoUri'), {
+            useNewUrlParser: true
+        })
+
+        app.listen(PORT, () => {
+            console.log(`Server has been started in port ${PORT}`);
+        })
+
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start()
