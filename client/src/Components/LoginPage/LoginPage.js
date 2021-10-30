950a3eeea0cc9745bd75d7310f57fab6cdf8055d
Elyor <elyor.sh.99@mail.ru>
2021-10-30 15:13:14 +0300

    #3 есть верстка страницы авторизации

--- client/src/Components/LoginPage/LoginPage.js
+++ client/src/Components/LoginPage/LoginPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import classes from './LoginPage.module.css'
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
 
 function LoginPage() {
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+    }
     return (
-        <div>
-            LoginPage
+        <div className={classes.loginPageContainer}>
+            <form className={classes.loginPageWrapper} onSubmit={handleSubmit}>
+                <div className={classes.inputBox}>
+                   <TextField className={classes.input} id="name" name="name" label="Login" variant="standard" />
+                </div>
+                <div className={classes.inputBox}>
+                   <TextField className={classes.input} id="password" name="password" label="Password" variant="standard" />
+                </div>
+                <div className={classes.btnBox}>
+                    <Button className={classes.btn} variant="contained" type="submit">Submit</Button>
+                </div>
+            </form>
         </div>
     )
 }
