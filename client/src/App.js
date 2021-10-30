3ebe8ad8a0715dc702521602d33eb99d6f5d284a
Elyor <elyor.sh.99@mail.ru>
2021-10-30 14:52:35 +0300

    #2 есть страницы loginpage, error, dashboard а также router

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,17 @@
-
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Dashboard from './Components/Dashboard/Dashboard';
+import Error from './Components/Error/Error';
+import LoginPage from './Components/LoginPage/LoginPage';
 function App() {
   return (
     <>
-      
+      <Router>
+        <Switch>
+          <Route exact path="/" component={Dashboard} />
+          <Route path="/login" component={LoginPage} />
+          <Route component={Error} />
+        </Switch>
+      </Router>
     </>
   );
 }
