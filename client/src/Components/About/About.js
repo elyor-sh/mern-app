f536761998cd1f3461ab1b0963216ff1d2e8447a
elyor shodiyorov <elyor.sh@routeamgroup.com>
2021-11-29 18:38:03 +0300

    #28 есть страница about

--- client/src/Components/About/About.js
+++ client/src/Components/About/About.js
@@ -1,12 +1,24 @@
 import React from 'react'
 import AppBar from '../AppBar/AppBar'
+import classes from './About.module.css'
 
 function About() {
-    return (
-        <>
-          <AppBar />  
-        </>
-    )
+  return (
+    <>
+      <AppBar />
+      <div className={classes.container}>
+        <div className={classes.warning}>
+          This site is for practice only, so when registering do not enter the password that you often use !!!
+        </div>
+        <div className={classes.warning}>
+          Этот сайт только для практики поэтому при регистрации не вводите пароль который вы часто пользуете!!!
+        </div>
+        <div className={classes.success}>
+          My github repository <a href="https://github.com/prElyor?tab=repositories" target="_blank">Github</a>
+        </div>
+      </div>
+    </>
+  )
 }
 
 export default About
